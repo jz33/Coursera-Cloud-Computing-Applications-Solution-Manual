1247ab4a766354a17f6b2d7786cd1f314b853b22
Jun <junzhengrice@gmail.com>
2015-09-04 12:36:16 -0500

    Create TitleCount.java

---
+++ hw2/TitleCount.java
@@ -0,0 +1,104 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.*
+import java.util.*;
+/**
+ * Classic "Word Count"
+ */
+public class TitleCount extends Configured implements Tool {
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new TitleCount(), args);
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(this.getConf(), "Title Count");
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(TitleCountMap.class);
+        job.setReducerClass(TitleCountReduce.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(TitleCount.class);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static String readHDFSFile(String path, Configuration conf) throws IOException{
+        Path pt=new Path(path);
+        FileSystem fs = FileSystem.get(pt.toUri(), conf);
+        FSDataInputStream file = fs.open(pt);
+        BufferedReader buffIn=new BufferedReader(new InputStreamReader(file));
+
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while( (line = buffIn.readLine()) != null) {
+            everything.append(line);
+            everything.append("\n");
+        }
+        return everything.toString();
+    }
+    
+    public static class TitleCountMap extends Mapper<Object, Text, Text, IntWritable> {
+        Set<String> stopWords = new HashSet<String>();
+        String delimiters;
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+
+            Configuration conf = context.getConfiguration();
+            
+            String delimitersPath = conf.get("delimiters");
+            delimiters = readHDFSFile(delimitersPath, conf);
+            
+            String stopWordsPath = conf.get("stopwords");
+            List<String> stopWordsList = Arrays.asList(readHDFSFile(stopWordsPath, conf).split("\n"));
+            for(String e : stopWordsList){
+                stopWords.add(e);
+            }
+        }
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer stk = new StringTokenizer(value.toString(),delimiters);
+            while(stk.hasMoreTokens()){
+                String e = stk.nextToken().trim().toLowerCase();
+                if(stopWords.contains(e) == false){
+                    context.write(new Text(e),new IntWritable(1));
+                }
+            }
+        }
+    }
+
+    public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for(IntWritable e : values){
+                sum += e.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+}
