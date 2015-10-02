85f2b763e5f41c612f4e8b3e70582a762b2e4f73
Jun <junzhengrice@gmail.com>
2015-10-02 13:11:09 -0500

    Create ShortestPathsComputation.java

---
+++ hw5/ShortestPathsComputation.java
@@ -0,0 +1,61 @@
+import org.apache.giraph.graph.BasicComputation;
+import org.apache.giraph.conf.LongConfOption;
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import java.io.IOException;
+/*
+
+0 - 1
+ \ /
+  2
+ / \
+3 - 4 - 5
+ \ /
+  6
+
+7 - 8
+ \ /  
+  9
+*/
+public class ShortestPathsComputation extends BasicComputation<
+    IntWritable, IntWritable, NullWritable, IntWritable>{   
+    
+    /*
+        Determine source vertex.
+        This is used at "-ca ShortestPathsComputation.sourceId=3"
+    */
+    public static final LongConfOption SOURCE_ID =
+        new LongConfOption("ShortestPathsComputation.sourceId", 1,
+        "The shortest paths id");
+
+    public boolean isSource(Vertex<IntWritable, ?, ?> vertex){
+        return vertex.getId().get() == SOURCE_ID.get(getConf());
+    }
+
+    @Override
+    public void compute(
+        Vertex<IntWritable, IntWritable, NullWritable> vertex,
+        Iterable<IntWritable> messages) throws IOException{
+            
+        if(getSuperstep() == 0){
+            vertex.setValue(new IntWritable(Integer.MAX_VALUE));
+        }
+        
+        int minDist = isSource(vertex) ? 0 : Integer.MAX_VALUE;
+        for (IntWritable message : messages){
+            minDist = Math.min(minDist, message.get());
+        }
+        
+        if (minDist < vertex.getValue().get()){
+            vertex.setValue(new IntWritable(minDist));
+            
+            for (Edge<IntWritable, NullWritable> edge : vertex.getEdges()){
+                int distance = minDist + 1;
+                sendMessage(edge.getTargetVertexId(), new IntWritable(distance));
+            }
+        }
+        vertex.voteToHalt();
+    }
+}
