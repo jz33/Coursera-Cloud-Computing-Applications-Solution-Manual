91c00769e7d9b7c1da2ea7e4998c127575311b16
Jun <junzhengrice@gmail.com>
2015-10-02 13:11:50 -0500

    Create run_c.sh

---
+++ hw5/run_c.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hadoop jar target/mp5-1.0-SNAPSHOT-jar-with-dependencies.jar org.apache.giraph.GiraphRunner ShortestPathsComputation -vif org.apache.giraph.io.formats.IntIntNullTextInputFormat -vip /mp5/data/graph.data  -vof org.apache.giraph.io.formats.IdWithValueTextOutputFormat -op /mp5/output/part-c -w 1 -ca giraph.SplitMasterWorker=false -ca ShortestPathsComputation.sourceId=3
